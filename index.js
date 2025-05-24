83624afd141886159bc3cc18f4d8f60983d2eb5a
Md Saiful Islam Rafel <mdsaifulisalmrafel.com>
2025-05-24 17:42:10 +0600

    .env file update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
   res.send("hello Developers");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5007;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
